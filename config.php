2c6460fc2e1e4f85e4a0438f9471b17f4c92d1ac
Orie Hulan <lexter@hotmail.ca>
2015-02-08 21:22:45 -0500

    Fixed subscribers list

--- config.php
+++ config.php
@@ -1,2 +1,3 @@
 <?php
-$dbc = new PDO('mysql:host=mysql5.loosefoot.com;dbname=gc200261414', ' gc200261414', 'Orie2015');
+$dbc = new PDO('mysql:host=localhost;dbname=php', 'root', '');
+// $dbc = new PDO('mysql:host=mysql5.loosefoot.com;dbname=gc200261414', ' gc200261414', 'Orie2015');
--- subscribers.php
+++ subscribers.php
@@ -1,48 +1,60 @@
 <!DOCTYPE html>
 <html>
+    <head>
+        <meta content="text/html; charset=utf-8" http-equiv="Content-Type">
+        <title>Subscriber List</title>
+        <link rel="stylesheet" href="style.css" type="text/css" />
+    </head>
 
-<head>
-<meta content="text/html; charset=utf-8" http-equiv="Content-Type">
-<title>Subscriber List</title>
-<link rel="stylesheet" href="style.css" type="text/css" />
-</head>
+    <body>
+        <a href="subscriber.php">Add new Subscriber</a>
+        <?php
+            require_once 'config.php';
 
-<body>
+            //1. connec
+            $conn = $dbc;
 
-<a href ="subscriber.php">Add new Subscriber</a>
-<?php
-require_once 'config.php';
+            //2. write sql query
+            $sql = "SELECT * FROM subscribers";
 
-//1. connec
-$conn = $dbc;
+            //3. execute the query and store the results
+            $result = $conn->query($sql);
 
-//2. write sql query
-$sql = "SELECT * FROM subscribers";
+            //4. start our html table / grid
+            ?>
+                <table>
+                    <tr>
+                        <th>First Name</th>
+                        <th>Last Name</th>
+                        <th>Email</th>
+                        <th>Edit</th>
+                    </tr>
+                    <?php
+                        //5. Loop through the date and print the rows and columns.
+                        foreach ($result as $row) {
+                            ?>
+                                <tr>
+                                    <td><?php echo $row['first_name']; ?></td>
+                                    <td><?php echo $row['last_name']; ?></td>
+                                    <td><?php echo $row['email']; ?></td>
+                                    <td>
+                                        <a href="subscriber.php?subscriber_id=<?php echo $row['subscriber_id']; ?>">Inspect</a>
+                                    </td>
+                                    <td>
+                                        <a href="delete-subscriber.php?subscriber_id=<?php echo $row['subscriber_id']?>"
+                                            onclick="return confirm(\'Are you sure you want to delete this subscriber?\');">Delete</a>
+                                    </td>
+                                </tr>
+                            <?php
+                        }
+                    ?>
+                </table>
+            <?php
 
-//3. execute the query and store the results
-$result = $conn->query($sql);
+            //6. Close the table
 
-//4. start our html table / grid
-echo '<table><tr><th>First Name</th><th>Last Name</th><th>Email</th><th>Edit</th></tr>';
-    <table>
-        <tr>
-            <th. First Name/th>
-                <th>Last Name</th>
-                <th>Email</th>
-                <th>Delete</th>
-        </tr>'; //5. Loop through the date and print the rows and columns. echo $row[$result as $row) { echo '
-        <tr>
-            <td>' . $row['first_name'] . '</td>
-            <td>' .$row['last_name'] . '</td>
-            <td>' .$row['email'] . '</td>
-        <td><a href="subscriber.php?subscriber_id=' . $row['subscriber_id']
-        <td><a href= "delete-subscriber.php?subscriber_id=' . $row['subscriber_id'] . '"onclick="return confirm(\'Are you sure you want to delete this subscriber?\');">Delete</a></td></tr>';
-}
-//6. Close the table
-
-//7. Disconnect
-$conn = null;
-
-?>
-</body>
+            //7. Disconnect
+            $conn = null;
+        ?>
+    </body>
 </html>
